b227255be28c7d05c99ec53b0c263d4c4bb4cff5
soul-dial <souleymanemrdiallo@gmail.com>
2025-05-01 18:06:20 -0600

    implementation des methodes deposit et withdraw de la class CurrentAccount

--- App/src/bank/CurrentAccount.java
+++ App/src/bank/CurrentAccount.java
@@ -13,8 +13,59 @@ public class CurrentAccount extends Account implements Transaction{
 
     public void getBalance() {
 
-
         System.out.println("BALANCE OF ACCOUNT : "+balance+"$");
+
+    }
+
+    @Override
+    public void deposit(double amount) {
+
+        if(amount< 0 ){
+
+            System.out.println("Enter a valid amount");
+
+        }else{
+
+           balance += amount;
+
+            System.out.println("Your deposit of "+ amount+"$"+" has been made successfully");
+
+            this.getBalance();
+
+        }
+    }
+
+    public void withdraw(double amount)
+    {
+
+       if(amount<0)
+       {
+
+           System.out.println("Enter a valid amount");
+       }
+       else
+       {
+           if (this.balance < amount)
+           {
+
+               System.out.println("Balance insufficant ");
+
+               this.getBalance();
+           }
+
+           else
+           {
+               balance -= amount;
+
+               System.out.println("Your withdraw of "+ amount+"$"+" has been made successfully");
+
+               this.getBalance();
+
+           }
+
+
+       }
+
 
     }
 }
