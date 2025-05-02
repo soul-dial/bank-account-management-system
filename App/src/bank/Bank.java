b1e65684486646bb3b6a4c7cac6015206a32b191
soul-dial <souleymanemrdiallo@gmail.com>
2025-05-02 17:14:39 -0600

    implementation de la methode createClient de la class Bank

--- App/src/bank/Bank.java
+++ App/src/bank/Bank.java
@@ -1,7 +1,46 @@
 package bank;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public abstract class Bank {
 
-    private Client client;
+    private static List<Client > clientList = new ArrayList<>();
+
+    private static List<SavingAcoount> savingAcoountsList = new ArrayList<>();
+
+    private static List<CurrentAccount> currentAccountList = new ArrayList<>();
+
+
+    private Client createClient (){
+
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName,lastName,phoneNumber,adress;
+
+        System.out.println("Enter your firstname : ");
+
+        firstName = scanner.nextLine().toUpperCase();
+
+        System.out.println("Enter your lastname : ");
+
+        lastName = scanner.nextLine().toUpperCase();
+
+        System.out.println("Enter your adress ");
+
+        adress = scanner.nextLine();
+
+        System.out.println("Enter your phonenumber");
+
+        phoneNumber = scanner.nextLine();
+
+
+        return new Client(firstName,lastName,phoneNumber,adress);
+
+    }
+
+
+
 
 }
