72246cec46104ec8a0ddc5edd2f7dcfff77c6f55
soul-dial <souleymanemrdiallo@gmail.com>
2025-05-04 16:45:48 -0600

    implementation des methodes equals et hashCode  de la class Client

--- App/src/bank/Client.java
+++ App/src/bank/Client.java
@@ -1,22 +1,40 @@
 package bank;
 
+import java.util.Objects;
+
 public class Client {
 
     private String firstName;
+    private String middleName;
     private String lastName;
     private String phonenumber;
     private String adresse;
 
-    Client(String firstName, String lastName, String phonenumber,String adresse ){
-
+    Client(String firstName, String middleName,String lastName, String phonenumber,String adresse ){
 
         this.firstName = firstName;
+
+        this.middleName = middleName;
 
         this.lastName = lastName;
 
         this.adresse = adresse;
 
         this.phonenumber = phonenumber;
+
+    }
+
+    Client(String firstName, String middleName,String lastName){
+
+        this.firstName = firstName;
+
+        this.middleName = middleName;
+
+        this.lastName = lastName;
+
+        this.adresse ="";
+
+        this.phonenumber ="";
 
     }
 
@@ -28,11 +46,44 @@ public class Client {
         return lastName;
     }
 
+    public String getMiddleName() {
+        return middleName;
+    }
+
     String getPhonenumber() {
         return phonenumber;
     }
 
     String getAdresse() {
         return adresse;
+    }
+
+
+    @Override
+     public boolean equals(Object obj)
+    {
+
+        if(this == obj) return true;
+
+        if ( !(obj == null) && (obj instanceof Client) )
+        {
+            Client c = (Client) obj;
+
+            if (this.firstName.equals(c.getFirstName())  && this.lastName.equals(c.getLastName())  && this.middleName.equals(c.getMiddleName()))
+            {
+               return true;
+
+            }
+
+        }
+
+        return false;
+    }
+
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(firstName,lastName,middleName);
     }
 }
