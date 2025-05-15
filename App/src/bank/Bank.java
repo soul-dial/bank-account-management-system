17ee35bf450f6c77d20e9e3b41fa6bb88f99fbb6
soul-dial <souleymanemrdiallo@gmail.com>
2025-05-14 23:45:14 -0600

    amelioration de l'interface

--- App/src/bank/Bank.java
+++ App/src/bank/Bank.java
@@ -59,7 +59,8 @@ public abstract class Bank {
 
      //contenation des chiffres du tableau en String
 
-       return ""+numbers[0]+numbers[1]+numbers[2]
+       return
+               ""+numbers[0]+numbers[1]+numbers[2]
               +"-"+numbers[3]+numbers[4]+numbers[5]
               +"-"+numbers[6]+numbers[7]+numbers[8];
 
